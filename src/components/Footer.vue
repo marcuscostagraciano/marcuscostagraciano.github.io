2801f2f0328d35294951d96700ad02b3c2f737dd
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-02-24 15:01:14 -0300

    early testing

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,23 @@
 <script setup>
+const contact_links = [
+    {url:"https://github.com/marcuscostagraciano", logo:"./assets/img/github.png"},
+    {url:"https://www.linkedin.com/in/marcus-costa-graciano/", logo:""},
+    {url:"https://marcuscostagraciano.github.io/portfolio/", logo:""},
+]
 
 </script>
 
 <template>
-    <div>
-
-    </div>
+    <footer>
+        <p v-for="link in contact_links">
+            {{ link.url }}
+        </p>
+    </footer>
 </template>
 
 <style scoped>
-
+    footer {
+        display: flex;
+        align-content: space-between;
+    }
 </style>
