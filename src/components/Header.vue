b8983c88d9f5a38db7da0dc32debfbd975dcb5dd
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-06 22:30:54 -0300

    mobile dropdown menu for pages

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,10 +16,6 @@ function checkScreenWidth() {
     isMobile.value = false
     mobileNav.value = false
   }
-
-  console.log("WINDOW WIDTH:", windowWidth.value)
-  console.log("ISMOBILE:", isMobile.value)
-  console.log("MOBILENAV:", mobileNav.value)
 }
 
 onBeforeMount(() => {
@@ -27,15 +23,12 @@ onBeforeMount(() => {
   checkScreenWidth()
 })
 
-function toggleMobileNav() {
-  mobileNav.value = !mobileNav.value
-  console.log(mobileNav.value)
-}
+// function toggleMobileNav() {
+//   mobileNav.value = !mobileNav.value
+//   console.log(mobileNav.value)
+// }
 
 </script>
-
-
-<!-- QUEBRA ATÉ 918 -->
 
 <template>
   <v-app-bar density="comfortable" scroll-behavior="hide" scroll-threshold="1">
@@ -45,6 +38,7 @@ function toggleMobileNav() {
       </v-app-bar-title>
     </v-col>
 
+    <!-- QUEBRA ATÉ 918 -->
     <v-col v-show="!isMobile" class="text-center text-md-right">
       <v-btn @click="$emit('change-Theme')" :icon="theme ? 'mdi-white-balance-sunny' : 'mdi-weather-night'"
         :color="theme ? 'yellow' : 'black'"></v-btn>
@@ -54,37 +48,28 @@ function toggleMobileNav() {
     </v-col>
 
     <v-col v-show="isMobile" class="text-right">
-      <v-btn icon="mdi-menu" @click="toggleMobileNav"></v-btn>
+      <v-btn @click="$emit('change-Theme')" :icon="theme ? 'mdi-white-balance-sunny' : 'mdi-weather-night'"
+        :color="theme ? 'yellow' : 'black'"></v-btn>
 
-      <ul v-show="mobileNav">
-        <li v-for="route in routes"><router-link :to="route.path">{{ route.name
-            }}</router-link></li>
-      </ul>
+      <v-menu location="bottom">
+        <template v-slot:activator="{ props }">
+          <v-btn color="primary" dark v-bind="props" icon="mdi-menu">
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="route in routes">
+            <v-list-item-title class="text-button text-center"><router-link :to="route.path">{{ route.name
+                }}</router-link></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-col>
 
   </v-app-bar>
 </template>
 
 <style scoped>
-ul {
-  display: block;
-  position: absolute;
-  z-index: 5;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 50vw;
-  overflow: visible;
-  background-color: yellow;
-}
-
-ul li {
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  list-style: none;
-}
-
 a {
   text-decoration: none;
 }
