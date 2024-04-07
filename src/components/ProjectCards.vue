13ed25cde3c6c7950d5bb153392b52693fd1391a
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-07 14:01:27 -0300

    projects card componentization

---
+++ src/components/ProjectCards.vue
@@ -0,0 +1,94 @@
+<script setup>
+defineProps({
+    projects: Object
+})
+</script>
+
+<template>
+
+    <v-col v-for="project in projects" cols="auto">
+        <v-card class="card mx-auto">
+            <v-card-item>
+                <v-card-subtitle>{{ project.creation_date }}
+                    <span v-if="project.language">- {{ project.language }}</span>
+                </v-card-subtitle>
+                <div>
+                    <div class="project-title text-h5 mb-1">
+                        {{ project.name }}
+                    </div>
+                    <div class="project-description text-caption">
+                        {{ project.description }}
+                    </div>
+                </div>
+            </v-card-item>
+
+            <v-card-actions class="action">
+                <a :href="project.url" target="_blank">
+                    <v-btn width="100%">
+                        Go to project!
+                    </v-btn>
+                </a>
+            </v-card-actions>
+        </v-card>
+    </v-col>
+
+</template>
+
+<style scoped>
+.card:not(:hover) .project-title {
+    opacity: 75%;
+}
+
+.card {
+    display: flex;
+    flex-direction: column;
+    align-self: center;
+    margin: auto;
+    /* word-break: auto-phrase; */
+    width: 85vw;
+    height: 100%;
+
+    @media screen and (min-width: 480px) {
+        width: 41.5vw;
+    }
+
+    @media screen and (min-width: 768px) {
+        width: 20vw;
+    }
+
+    @media screen and (min-width: 960px) {
+        width: 18vw;
+    }
+
+}
+
+.action {
+    background-color: rgb(0, 0, 238, 0.03);
+    margin-top: auto;
+}
+
+.project-title {
+    text-transform: capitalize !important;
+    font-size: 1.2em !important;
+}
+
+.project-description {
+    max-height: 3rem;
+    overflow: hidden;
+}
+
+.project-description:hover {
+    max-height: 5rem;
+    overflow-y: auto;
+}
+
+a {
+    height: 100%;
+    width: 100%;
+    text-decoration: none;
+}
+
+button:hover {
+    text-decoration: underline;
+}
+</style>
--- src/views/ProjectsListView.vue
+++ src/views/ProjectsListView.vue
@@ -1,7 +1,9 @@
 <script setup>
+
+import ProjectCards from '../components/ProjectCards.vue';
+
 import fetchGitHubRepos from '../api/github_repos';
 const gitUsername = 'marcuscostagraciano'
-
 const fetched_repos = await fetchGitHubRepos(gitUsername)
 
 </script>
@@ -9,33 +11,17 @@ const fetched_repos = await fetchGitHubRepos(gitUsername)
 <template>
     <h1 class="py-3">Projects page</h1>
 
-    <v-container>
-        <v-row>
-            <v-col v-for="project in  fetched_repos" cols="auto">
-                <v-card class="card mx-auto">
-                    <v-card-item>
-                        <v-card-subtitle>{{ project.creation_date }}
-                            <span v-if="project.language">- {{ project.language }}</span>
-                        </v-card-subtitle>
-                        <div>
-                            <div class="project-title text-h5 mb-1">
-                                {{ project.name }}
-                            </div>
-                            <div class="project-description text-caption">{{ project.description }}</div>
-                        </div>
-                    </v-card-item>
+    <Suspense>
+        <v-container>
+            <v-row>
+                <ProjectCards :projects="fetched_repos" />
+            </v-row>
+        </v-container>
 
-                    <v-card-actions>
-                        <a :href="project.url" target="_blank">
-                            <v-btn width="100%">
-                                Go to project!
-                            </v-btn>
-                        </a>
-                    </v-card-actions>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+        <template #fallback>
+            Loading
+        </template>
+    </Suspense>
 </template>
 
 <style scoped>
@@ -43,51 +29,5 @@ const fetched_repos = await fetchGitHubRepos(gitUsername)
     margin: auto;
     display: flex;
     align-content: center !important;
-}
-
-.card:not(:hover) .project-title {
-    opacity: 75%;
-}
-
-.card {
-    display: flex;
-    flex-direction: column;
-    align-self: center;
-    margin: auto;
-    word-break: auto-phrase;
-    width: 85vw;
-
-    @media screen and (min-width: 480px) {
-        width: 41.5vw;
-    }
-
-    @media screen and (min-width: 768px) {
-        width: 20vw;
-    }
-
-    @media screen and (min-width: 960px) {
-        width: 18vw;
-    }
-
-}
-
-.project-title {
-    text-transform: capitalize !important;
-    max-height: 100pxrem;
-}
-
-.project-description {
-    max-height: 5rem;
-    overflow: hidden;
-}
-
-a {
-    height: 100%;
-    width: 100%;
-    text-decoration: none;
-}
-
-button:hover {
-    text-decoration: underline;
 }
 </style>
